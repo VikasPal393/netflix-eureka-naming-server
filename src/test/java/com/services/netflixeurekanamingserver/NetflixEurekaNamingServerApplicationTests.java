a18ecaf9f6bfffa6edb0484e69635b732e3d6d9f
Vikas <vikas.pal.8445@gmail.com>
2020-02-09 12:08:14 +0530

    first commit

---
+++ src/test/java/com/services/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
